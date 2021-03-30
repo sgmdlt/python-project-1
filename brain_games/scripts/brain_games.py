eec2862808bb247569c582c3642157202a4b38f1
Ivan Mamtsev <justgodshot@gmail.com>
2021-03-30 22:54:34 +0300

    added cli prompt

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+from brain_games.cli import welcome_user
+
+
 def greet():
     print('Welcome to the Brain Games!')
 
 
 def main():
     greet()
+    welcome_user()
 
 
 if __name__ == '__main__':
