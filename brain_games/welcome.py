d9a8965d8614fb4605be1b285d287e6983250e24
Ivan Mamtsev <justgodshot@gmail.com>
2021-04-16 01:46:04 +0300

    added docstring

--- brain_games/welcome.py
+++ brain_games/welcome.py
@@ -3,6 +3,7 @@ import prompt
 
 
 def welcome():
+    '''Display welcome message and return user name'''
     print('Welcome to Brain Games!')
     user_name = prompt.string('May I have your name?\n')
     print('Hello, {}!'.format(user_name))
