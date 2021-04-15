9e49e2baa07544598c4a51f273cc5d506ce7a687
Ivan Mamtsev <justgodshot@gmail.com>
2021-04-16 01:43:47 +0300

    added "brain-progression" game

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.games.game_progression import game_progression
+from brain_games.scripts.engine import engine
+
+
+def main():
+    engine(game_progression)
+
+
+if __name__ == '__main__':
+    main()
