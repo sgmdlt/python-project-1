2a025ed48df94f4b6f10297c63e68f6825964d57
Ivan Mamtsev <justgodshot@gmail.com>
2021-04-16 01:45:25 +0300

    added docstring

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -3,6 +3,14 @@ import random
 
 
 def game_even():
+    '''
+    Display random number and return its parity
+
+    Return
+    expression -- chosen at random number. (ex. '12')
+    correct_ansewr -- 'yes' or 'no' depending on parity (ex. 'yes')
+    '''
+
     print('Answer "yes" if the number is even, otherwise answer "no".')
     expression = random.randint(1, 16)
     if expression % 2 == 0:
@@ -12,5 +20,5 @@ def game_even():
     return expression, correct_answer
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     game_even()
