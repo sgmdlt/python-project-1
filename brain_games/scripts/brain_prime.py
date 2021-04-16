62fd9139e1ccef9c79c69859a2236d1977565843
Ivan Mamtsev <justgodshot@gmail.com>
2021-04-17 01:44:06 +0300

    added "brain-prime" game

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.games.game_prime import game_prime
+from brain_games.engine import engine
+
+
+def main():
+    engine(game_prime)
+
+
+if __name__ == '__main__':
+    main()
