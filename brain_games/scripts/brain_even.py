cabf4d91c0af6d90af077316c743111e98b1b666
Ivan Mamtsev <justgodshot@gmail.com>
2021-04-16 23:43:09 +0300

    edited "engine" import path

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from brain_games.games.game_even import game_even
-from brain_games.scripts.engine import engine
+from brain_games.engine import engine
 
 
 def main():
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from brain_games.games.game_gcd import game_gcd
-from brain_games.scripts.engine import engine
+from brain_games.engine import engine
 
 
 def main():
