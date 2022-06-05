f5bdd2f1c5aa51cf3a507c17abbd98f12cb4d431
sgmdlt <user@usermail.com>
2022-06-06 02:12:25 +0400

    refactoring game_calc

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,31 +1,28 @@
 import operator
 import random
 
-OPERATORS = {
+OPERATORS = {  # noqa: WPS407
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
 }
 RAND_MIN = 1
 RAND_MAX = 20
-GAME_MESSAGE = 'What is the result of the expression?'
+GAME_MESSAGE = 'What is the result of the question?'
 
 
-def game_task():
-    '''
-    Display arithmetic expression and return its result
+def game_task():  # noqa: WPS210
+    """Display arithmetic question and return its result.
 
     Return:
-    expression -- string in the format 'first_number sign second_number',
+    question -- string in the format 'first_number sign second_number',
     where first_number, sign, second_number are chosen at random. (ex. '3 + 8')
-    correct_answer -- result of chosen expression in 'str' format. (ex. '11')
-    '''
-
+    correct_answer -- result of chosen question in 'str' format. (ex. '11')
+    """
     first_number = random.randint(RAND_MIN, RAND_MAX)
     second_number = random.randint(RAND_MIN, RAND_MAX)
-    (sign, op) = random.choice(list(OPERATORS.items()))
+    sign, op = random.choice(list(OPERATORS.items()))
 
-    expression = '{first} {s} {second} = '.format(
-        first=first_number, s=sign, second=second_number)
+    question = f'{first_number} {sign} {second_number} = '
     correct_answer = str(op(first_number, second_number))
-    return expression, correct_answer
+    return question, correct_answer
