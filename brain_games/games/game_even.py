19ba3b818681c6667fc3a78309c4264a395ebf2d
sgmdlt <user@usermail.com>
2022-06-06 02:22:53 +0400

    refactoring game_even

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -1,23 +1,24 @@
 import random
 
-
 RAND_MIN = 1
 RAND_MAX = 20
 GAME_MESSAGE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    return number % 2 == 0
+
+
 def game_task():
-    '''
-    Display random number and return its parity
+    """Display random number and return its parity.
 
     Return
-    expression -- chosen at random number. (ex. '12')
+    number -- chosen at random number. (ex. '12')
     correct_ansewr -- 'yes' or 'no' depending on parity (ex. 'yes')
-    '''
-
-    expression = random.randint(RAND_MIN, RAND_MAX)
-    if expression % 2 == 0:
+    """
+    number = random.randint(RAND_MIN, RAND_MAX)
+    if is_even(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    return expression, correct_answer
+    return number, correct_answer
