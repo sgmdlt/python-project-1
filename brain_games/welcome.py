09f956bfbe7b70e5c48bb882e4958c64e4dabd20
sgmdlt <user@usermail.com>
2022-06-06 03:55:38 +0400

    fix

--- brain_games/welcome.py
+++ brain_games/welcome.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
 import prompt
 
 
 def welcome():
-    '''Display welcome message and return user name'''
+    """Display welcome message and return user name."""
     print('Welcome to Brain Games!')
     user_name = prompt.string('May I have your name?\n')
-    print('Hello, {}!'.format(user_name))
+    print(f'Hello, {user_name}!')
     return user_name
