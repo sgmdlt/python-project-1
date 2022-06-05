e3616696c2d82f658c2c01f8f1986b1a65f58499
sgmdlt <user@usermail.com>
2022-06-06 02:27:03 +0400

    refactoring game_gcd

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -1,36 +1,32 @@
 import random
 
-
 RAND_MIN = 1
 RAND_MAX = 10
 GAME_MESSAGE = 'Find the greatest common divisor of given numbers.'
 
 
 def find_gcd(first_number, second_number):
-    '''Find GCD of two numbers with Euclidean division algorithm'''
+    """Find GCD of two numbers with Euclidean division algorithm."""
     while first_number != second_number:
         if first_number > second_number:
-            first_number = first_number - second_number
+            first_number -= second_number
         else:
-            second_number = second_number - first_number
+            second_number -= first_number
     return first_number
 
 
 def game_task():
-    '''
-    Display two random numbers and return their GCD.
+    """Display two random numbers and return their GCD.
 
     Return
-    expression -- string with two random numbers. (ex. '42 30')
+    question -- string with two random numbers. (ex. '42 30')
     correct_answer -- their GCD in 'str' format. (ex. '6')
-    '''
-
+    """
     base_divisor = random.randint(RAND_MIN, RAND_MAX)
     first_number = base_divisor * random.randint(RAND_MIN, RAND_MAX)
     second_number = base_divisor * random.randint(RAND_MIN, RAND_MAX)
-    expression = '{0} {1}'.format(first_number, second_number)
 
-    gcd = find_gcd(first_number, second_number)
-    correct_answer = str(gcd)
+    question = f'{first_number} {second_number}'
+    correct_answer = str(find_gcd(first_number, second_number))
 
-    return expression, correct_answer
+    return question, correct_answer
